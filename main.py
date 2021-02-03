f07b2177a593146fc7fa2a29b77520d9eab9afaf
simon38514258 <78457309+simon38514258@users.noreply.github.com>
2021-02-03 15:22:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def index():
         messages = [
             {
                 "type":"text",
-                "text":"你給我閉嘴"
+                "text":"你給我閉嘴1"
             }
         ]
 
@@ -28,4 +28,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1',port=5000,debug=True, use_reloader=False)
-#
+
