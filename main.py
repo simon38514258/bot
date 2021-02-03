8c9918421f21d4de83f27940b4e83b2188a405be
simon38514258 <78457309+simon38514258@users.noreply.github.com>
2021-02-03 14:30:22 +0800

    linebot setup

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ from flask import Flask,request,render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return 'succeed'
+        if request.method == "POST":
+        message = request.get_json().get("events")[0]
+        print(message)
+        return "succeed"
+    elif request.method == "GET":
+        return "succeed"
+    else:
+        return "succeed"
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1',port=5000,debug=True, use_reloader=False)
+if __name__ == "__main__":
+    app.run(host="127.0.0.1",port=5000,debug=True, use_reloader=False)
 
