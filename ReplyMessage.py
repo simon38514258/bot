0de0ba0dbf576253bfd7bc51badd847cb475fe69
simon38514258 <78457309+simon38514258@users.noreply.github.com>
2021-02-03 15:08:10 +0800

    .

---
+++ ReplyMessage.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+def ReplyMessage(replyToken,messages):
+    #請使用自己的token
+    accessToken = "l7llJ3W9l369bgoXjKeyl0v5b5JPvYWQh6RYkISoit71uat3iubKKG2yC / GL / F9qLF00K5xENA3hxsZUkk / A + 4Ud4JJiMQbOCgKero9XVVWWzKdiabn + nsXTOPK1KVoZZTpKVxZXKP3CZ1P1E1A3C"
+    
+    headers ={
+        'Content-Type':'application/json',
+        'Authorization': 'Bearer '+accessToken
+    }
+    data ={
+        "replyToken":replyToken,
+        'messages':messages
+    }
+    url = 'https://api.line.me/v2/bot/message/reply'
+    r = requests.post(url,headers=headers,data=json.dumps(data))
+    return r
--- main.py
+++ main.py
@@ -1,18 +1,31 @@
-from flask import Flask,request,render_template
+from flask import Flask,request
+from Modules.ReplyMessage import ReplyMessage
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+#HTTP請求方式
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == "POST":
-        message = request.get_json().get("events")[0]
+    if request.method == 'POST':
+        message = request.get_json().get('events')[0]
         print(message)
-        return "succeed"
-    elif request.method == "GET":
-        return "succeed"
-    else:
-        return "succeed"
+        replyToken = message.get('replyToken')
 
-if __name__ == "__main__":
-    app.run(host="127.0.0.1",port=5000,debug=True, use_reloader=False)
+        messages = [
+            {
+                "type":"text",
+                "text":"Hello, world1"
+            }
+        ]
+
+        ReplyMessage(replyToken,messages)
+
+        return 'succeed'
+    elif request.method == 'GET':
+        return 'succeed'
+    else:
+        return 'succeed'
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1',port=5000,debug=True, use_reloader=False)
 
