53d4170d43e4ac7d409a966a2c43636b9be0e81b
simon38514258 <78457309+simon38514258@users.noreply.github.com>
2021-02-03 15:16:10 +0800

    .

--- Modules/ReplyMessage.py
+++ Modules/ReplyMessage.py
@@ -3,7 +3,7 @@ import json
 
 def ReplyMessage(replyToken,messages):
     #請使用自己的token
-    accessToken = "l7llJ3W9l369bgoXjKeyl0v5b5JPvYWQh6RYkISoit71uat3iubKKG2yC / GL / F9qLF00K5xENA3hxsZUkk / A + 4Ud4JJiMQbOCgKero9XVVWWzKdiabn + nsXTOPK1KVoZZTpKVxZXKP3CZ1P1E1A3C"
+    accessToken = "l7llJ3W9l369bgoXjKeyl0v5b5JPvYWQh6RYkISoit71uat3iubKKG2yC/GL/F9qLF00K5xENA3hxsZUkk/A+4Ud4JJiMQbOCgKero9XVhWWxKdiabn+nsXTOPk3RVzGdPmpqAeFzV+zgK+GOxXXAAdB04t89/1O/w1cDnyilFU="
     
     headers ={
         'Content-Type':'application/json',
