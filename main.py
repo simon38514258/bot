cea95bb44b8244b4c8062d9904da3a743f7e7768
simon38514258 <78457309+simon38514258@users.noreply.github.com>
2021-02-04 10:23:44 +0800

    2

--- main.py
+++ main.py
@@ -17,36 +17,27 @@ def index():
         if userMessage == "剪刀":
             AI = random.randint(0,2)
             if AI == 0:
-                print("剪刀")
-                result = "平手"
+                result = "剪刀"/n"平手"
             elif AI == 2:
-                print("布")
-                result = "你贏了"
+                result = "布"/n"你贏了"
             else:
-                print("石頭")
-                result = "你輸了"
+                result = "石頭"/n"你輸了"
         elif userMessage == "石頭":
             AI = random.randint(0,2)
             if AI == 1:
-                print("石頭")
-                result = "平手"
+                result = "石頭"/n"平手"
             elif AI == 0:
-                print("剪刀")
-                result = "你贏了"
+                result = "剪刀"/n"你贏了"
             else:
-                print("布")
-                result = "你輸了"
+                result = "布"/n"你輸了"
         elif userMessage == "布":
             AI = random.randint(0,2)
             if AI == 2:
-                print("布")
-                result = "平手"
+                result = "布"/n"平手"
             elif AI == 1:
-                print("石頭")
-                result = "你贏了"
+                result = "石頭"/n"你贏了"
             else:
-                print("剪刀")
-                result = "你輸了"
+                result = "剪刀"/n"你輸了"
         else:
             if userMessage == "嗨":#message.get("message").get("type") 
                 messages = [
