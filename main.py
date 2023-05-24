55a4035816b8e06ea3a2f8b0b54f5e4fd75da609
simon38514258 <78457309+simon38514258@users.noreply.github.com>
2023-05-24 15:49:55 +0800

    go

--- main.py
+++ main.py
@@ -274,3 +274,13 @@ def api():
     with open('data.json', mode='r') as my_file:
         text = my_file.read()
         return text
+ 
+ 
+class handler(BaseHTTPRequestHandler):
+ 
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!'.encode('utf-8'))
+        return
